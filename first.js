51dd706eda8bac6df88e1a363f2824c3fe738695
Precious Bassey <preciousasuquo6@gmail.com>
2022-06-23 13:41:13 +0100

    first JavaScript task

---
+++ first.js
@@ -0,0 +1,7 @@
+let Name = "Precious";
+let height = "186cm";
+let country ="Nigeria";
+
+console.log("My name is " + Name);
+console.log("I am " + height + " tall");
+console.log("I am from " + country);
